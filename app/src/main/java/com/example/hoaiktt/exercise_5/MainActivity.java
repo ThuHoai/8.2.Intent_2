3c4d0013f388dd75ae0b007e35d563c7c20040c7
ThuHoai <hoaiuct95@gmail.com>
2017-07-26 10:09:32 +0700

    Rejected commit: Default

--- app/src/main/java/com/example/hoaiktt/exercise_5/MainActivity.java
+++ app/src/main/java/com/example/hoaiktt/exercise_5/MainActivity.java
@@ -6,16 +6,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mBtnShowSumActivity;
-    public static String mAddress = "sum://example.com";
+    private Button mBtnShowSumActivity, mBtnShowWithData;
+    private String mAddress = "sum://example.com";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBtnShowSumActivity = (Button) findViewById(R.id.btnShowSumActivity);
         mBtnShowSumActivity.setOnClickListener(new CallSumActivity());
+        mBtnShowWithData = (Button) findViewById(R.id.btnShowWithData);
+        mBtnShowWithData.setOnClickListener(new SendDataToSumActivity());
+    }
+
+    private String makeDataRamdomToSumActivity(){
+        String value_X = String.format("X=%s", String.valueOf(Math.random()));
+        String value_Y = String.format("Y=%s", String.valueOf(Math.random()));
+        return String.format("%s?%s&%s",mAddress,value_X,value_Y);
+    }
+
+    private String makeAddressToSumActivity(){
+        EditText edtXInput = (EditText) findViewById(R.id.edtX);
+        EditText edtYInput = (EditText) findViewById(R.id.edtY);
+        String value_X = String.format("X=%s", edtXInput.getText().toString());
+        String value_Y = String.format("Y=%s", edtYInput.getText().toString());
+        return String.format("%s?%s&%s",mAddress,value_X,value_Y);
     }
 
     private class CallSumActivity implements View.OnClickListener{
@@ -27,4 +44,16 @@ public class MainActivity extends AppCompatActivity {
                startActivity(mItent);
         }
     }
+
+    private class SendDataToSumActivity implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            Uri mUriSumActivity2 = Uri.parse(makeAddressToSumActivity());
+            Intent mData = new Intent(Intent.ACTION_VIEW,mUriSumActivity2);
+            startActivity(mData);
+        }
+    }
+
+
 }
